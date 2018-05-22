852d304c2daad310a0f79efcfbe4378d36e40104
Quentin Labourey <quentin.labourey@laas.fr>
2018-05-22 18:47:05 +0200

    (Splitted from CDFF_at_LAAS)[TOKAMAK][CIRCULARMAP] Changes to explicit types

--- src/circularMap.cpp
+++ src/circularMap.cpp
@@ -1,60 +1,72 @@
 #include <circularMap.hpp>
 
+namespace tokamak{
 
-circularMap::circularMap()
-{
-    this->size = DEFAULT_SIZE;
-}
-
-circularMap::circularMap(int size)
-{
-    this->size = size;
-}
-
-iterator circularMap::begin() 
-{
-    return map_.begin(); 
-}
-
-iterator circularMap::end() 
-{
-    return map_.end();
-}
-
-iterator circularMap::find_upper(K key)
-{
-    return map_.upper_bound(key);
-}
-
-iterator circularMap::find_lower(K key)
-{
-    return map_.lower_bound(key);
-}
-
-iterator circularMap::find(K key)
-{
-    return map_.find(key);
-}
-
-bool circularMap::empty()
-{
-    return map_.empty();
-}
-
-void circularMap::put(K k, V v)
-{ 
-	map_.insert(std::make_pair(k,v));
-	deque_.push_back(k);
-	_ensure();  
-}
-
-
-void circularMap::_ensure() 
-{ 
-	if (deque_.size() > this->size) 
-	{ 
-		map_.erase(deque_.front()); 
-		deque_.pop_front();
-	}
-}
+    template <typename K, typename V>
+    circularMap<K,V>::circularMap()
+    {
+        this->size = DEFAULT_SIZE;
+    }
+    
+    template <typename K, typename V>
+    circularMap<K,V>::circularMap(int size)
+    {
+        this->size = size;
+    }
+    
+    template <typename K, typename V>
+    typename std::map<K,V>::iterator circularMap<K,V>::begin() 
+    {
+        return map_.begin(); 
+    }
+    
+    template <typename K, typename V>
+    typename std::map<K,V>::iterator circularMap<K,V>::end() 
+    {
+        return map_.end();
+    }
+    
+    template <typename K, typename V>
+    typename std::map<K,V>::iterator circularMap<K,V>::find_upper(K key)
+    {
+        return map_.upper_bound(key);
+    }
+    
+    template <typename K, typename V>
+    typename std::map<K,V>::iterator circularMap<K,V>::find_lower(K key)
+    {
+        return map_.lower_bound(key);
+    }
+    
+    template <typename K, typename V>
+    typename std::map<K,V>::iterator circularMap<K,V>::find(K key)
+    {
+        return map_.find(key);
+    }
+    
+    template <typename K, typename V>
+    bool circularMap<K,V>::empty()
+    {
+        return map_.empty();
+    }
+    
+    template <typename K, typename V>
+    void circularMap<K,V>::put(K k, V v)
+    { 
+    	map_.insert(std::make_pair(k,v));
+    	deque_.push_back(k);
+    	_ensure();  
+    }
+    
+    
+    template <typename K, typename V>
+    void circularMap<K,V>::_ensure() 
+    { 
+    	if (deque_.size() > this->size) 
+    	{ 
+    		map_.erase(deque_.front()); 
+    		deque_.pop_front();
+    	}
+    }
+};
 
