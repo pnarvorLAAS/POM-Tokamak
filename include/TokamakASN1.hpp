423a53df78d7369ce2c31c979da1de8e51c07898
Quentin Labourey <quentin.labourey@laas.fr>
2018-05-22 16:30:52 +0200

    (Splitted from CDFF_at_LAAS)[TOKAMAK] Wrote TokamakASN1. Compiling and ready to be tested

--- include/TokamakASN1.hpp
+++ include/TokamakASN1.hpp
@@ -2,25 +2,40 @@
 #define __TOKAMAK_ASN1_HPP__
 
 #include <Pose_InFuse.h>
+#include <Tokamak.hpp>
+#include <conversions/asn1_pom_conversions.hpp>
 
-namespace tokamak{
+#define e_asn1  Error("Message must be wrong")
 
+namespace tokamak
+{
     class TokamakASN1: public Tokamak
     {
         private:
-            Pose_InFuse*    poseInFuseInput;
-            Pose_InFuse*    poseInFuseOutput;
+            Pose_InFuse*    poseInFusePublish;
+            Pose_InFuse*    poseInFuseRespond;
+            Pose_InFuse*    poseInFuseInsert;
+            Pose_InFuse*    poseInFuseRequest;
             byte*           perBuffer;
 
         public:
             TokamakASN1();
-            ~TokamaASN1();
+            ~TokamakASN1();
             void clean_up();
-            
-            bool decode_message(BitStream msg);
-            bool update_pose(/*poseInFuseInput*/);
-            bool update_outputMsg(/*fusedMap,demMsgOutput*/);
-            BitStream encode_message(/*demMsgOutput*/);
+
+
+            // Wrappers of conversion messages
+            Pose_InFuse update_publish();
+            Pose_InFuse update_respond();
+
+            // Conversion to BitStream
+            BitStream encode_publish();
+            BitStream encode_response();
+
+            // Decoding from ASN1 BitStream
+            void decode_insertPose(BitStream &msg, PositionManager::Pose &poseToDecode);
+            void decode_request(BitStream &msg, PositionManager::Pose &poseToDecode);
+
 
     };
 };
--- src/TokamakASN1.cpp
+++ src/TokamakASN1.cpp
@@ -2,14 +2,129 @@
 
 namespace tokamak{
 
-    Tokamak::TokamakASN1()
+    TokamakASN1::TokamakASN1()
     {
-        poseInFuseInput
+        poseInFuseInsert    = new Pose_InFuse;
+        poseInFuseRespond   = new Pose_InFuse;
+        poseInFusePublish   = new Pose_InFuse;
+        poseInFuseRequest   = new Pose_InFuse;
+
+        perBuffer = new byte[Pose_InFuse_REQUIRED_BYTES_FOR_ENCODING];
+        memset(perBuffer,0,Pose_InFuse_REQUIRED_BYTES_FOR_ENCODING);
     }
 
-    Tokamak::~TokamakASN1()
+    TokamakASN1::~TokamakASN1()
     {
+        clean_up();
     }
+
+    void TokamakASN1::clean_up()
+    {
+        std::cout << "Cleaning up Tokamak ASN1" << std::endl;
+        delete perBuffer;
+        delete poseInFuseInsert;
+        delete poseInFuseRespond;
+        delete poseInFusePublish;
+        delete poseInFuseRequest;
+    }
+
+    Pose_InFuse TokamakASN1::update_publish()
+    {
+        toASN1SCC(posePublish,*poseInFusePublish);
+        return *poseInFusePublish;
+    }
+
+    Pose_InFuse TokamakASN1::update_respond()
+    {
+        toASN1SCC(poseRespond,*poseInFuseRespond);
+        return *poseInFuseRespond;
+    }
+
+    BitStream TokamakASN1::encode_publish()
+    {
+        //Convert the pose to publish from internal to ASN1 C type
+        toASN1SCC(posePublish,*poseInFusePublish);
+
+        //Convert the pose to publish from ASN1 C type to ASN1 BitStream
+        int errorCode;
+        BitStream b;
+        BitStream_Init(&b,perBuffer,Pose_InFuse_REQUIRED_BYTES_FOR_ENCODING);
+        try
+        {
+            if (!Pose_InFuse_Encode(poseInFusePublish,&b,&errorCode,TRUE))
+            {
+                throw e_asn1;
+            }
+        }
+        catch (std::exception const & e)
+        {
+            std::cerr << "[ENCODING FAILED]: " << e.what() << std::endl;
+        }
+        return b;
+    }
+    
+    BitStream TokamakASN1::encode_response()
+    {
+        //Convert the pose to publish from internal to ASN1 C type
+        toASN1SCC(poseRespond,*poseInFuseRespond);
+
+        //Convert the pose to publish from ASN1 C type to ASN1 BitStream
+        int errorCode;
+        BitStream b;
+        BitStream_Init(&b,perBuffer,Pose_InFuse_REQUIRED_BYTES_FOR_ENCODING);
+        try
+        {
+            if (!Pose_InFuse_Encode(poseInFuseRespond,&b,&errorCode,TRUE))
+            {
+                throw e_asn1;
+            }
+        }
+        catch (std::exception const & e)
+        {
+            std::cerr << "[ENCODING FAILED]: " << e.what() << std::endl;
+        }
+        return b;
+    }
+    
+    void TokamakASN1::decode_insertPose(BitStream &msg,PositionManager::Pose& poseToDecode)
+    {
+        int errorCode;
+        try 
+        {
+            if (!Pose_InFuse_Decode(poseInFuseInsert,&msg,&errorCode))
+            {
+                throw e_asn1;
+            }
+
+            // Transform to Internal format
+
+            fromASN1SCC(*poseInFuseInsert,poseToDecode);
+        }
+        catch (std::exception const& e)
+        {
+            std::cerr << "[DECODING FAILED]: " << e.what() << std::endl;
+        }
+    }
+    
+    void TokamakASN1::decode_request(BitStream &msg, PositionManager::Pose& poseToDecode)
+    {
+        int errorCode;
+        try 
+        {
+            if (!Pose_InFuse_Decode(poseInFuseRequest,&msg,&errorCode))
+            {
+                throw e_asn1;
+            }
+
+            // Transform to Internal format
+
+            fromASN1SCC(*poseInFuseRequest,poseToDecode);
+        }
+        catch (std::exception const& e)
+        {
+            std::cerr << "[DECODING FAILED]: " << e.what() << std::endl;
+        }
+    }
+
 };
 
-#endif
